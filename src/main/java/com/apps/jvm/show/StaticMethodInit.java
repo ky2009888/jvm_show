212c419d43cab810cf1c57ecf428b74b8fde3e24
ky2009888 <ji_jinliang2012@163.com>
2021-03-25 15:22:12 +0800

    Initial commit

---
+++ src/main/java/com/apps/jvm/show/StaticMethodInit.java
@@ -0,0 +1,32 @@
+package com.apps.jvm.show;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author ky2009666
+ * @Description 静态变量初始化
+ * @Date 2021/3/25
+ **/
+@Slf4j
+public class StaticMethodInit {
+    /**
+     * 定义变量num1.
+     */
+    private static int num1 = 5;
+
+    static {
+        num1 = 10;
+        //可以赋值，但是不能调用
+        num2 = 15;
+    }
+
+    /**
+     * 定义变量num2.
+     */
+    private static int num2 = 2;
+
+    public static void main(String[] args) {
+        //最终打印结果是2
+        log.info(String.valueOf(StaticMethodInit.num2));
+    }
+}
